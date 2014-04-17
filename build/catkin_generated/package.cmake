7da9013b1d5ffd9b069fa6aa7c3c1e5d9b97bcef
Paul Mathieu <paul.mathieu@pal-robotics.com>
2014-04-17 15:37:18 +0200

    Remove build folder

--- build/catkin_generated/package.cmake
+++
@@ -1,7 +0,0 @@
-set(_CATKIN_CURRENT_PACKAGE "libsiftfast")
-set(libsiftfast_MAINTAINER "student <student@todo.todo>")
-set(libsiftfast_DEPRECATED "")
-set(libsiftfast_VERSION "1.0.0")
-set(libsiftfast_BUILD_DEPENDS )
-set(libsiftfast_RUN_DEPENDS )
-set(libsiftfast_BUILDTOOL_DEPENDS "catkin")
